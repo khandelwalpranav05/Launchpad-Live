2f1ce31edb0033318394f68b406eed2508028101
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-07 03:18:49 +0530

    basics

---
+++ Lecture 1/condition_statements.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // Why ?
+
+using namespace std; // Why ?
+
+int main() { // What is ?
+
+	int wind = 0; // wind = 0;
+	int friends = 12;
+
+	if (wind == 1) {
+		cout << "We Can't Play";
+		cout << endl;
+	} else {
+
+		if (friends > 0) {
+			cout << "We Can Play";
+			cout << endl;
+
+		}
+	}
+
+	return 0; // Why ?
+}
---
+++ Lecture 1/loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // Why ?
+
+using namespace std; // Why ?
+
+int main() { // What is ?
+
+	int pos = 7;
+
+	while (pos < 10) {
+
+		pos = pos + 1;
+
+	}
+
+	cout << "Reached";
+	cout << endl;
+
+	return 0; // Why ?
+}
