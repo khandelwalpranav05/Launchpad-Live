30df01cca1cac8dc6dc6be20673724f350e380ff
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-13 20:08:12 +0530

    pattern

---
+++ Lecture 3/basic_doubts.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	// int i = 10;
+
+	// i++; //i = i +1;
+
+	// cout << (i) << endl; //10
+
+	// // cout << (i++) << endl; // 10
+	// cout << (++i) << endl; //
+
+	// cout << (i) << endl; //
+
+	// for (int i = 1; i < 5; i++) {
+	// 	cout << i << " ";
+	// }
+	// cout << endl;
+
+	// cout << "********************" << endl;
+
+	// for (int i = 1; i < 5; ++i) {
+	// 	cout << i << " ";
+	// }
+	// cout << endl;
+
+	// int i = 1;
+
+	// while (i < 5) {
+	// 	cout << i << " ";
+	// 	i++;
+	// }
+	// cout << endl;
+
+	// int i = 1;
+	// for (; i < 5;) {
+
+	// 	cout << i << " ";
+
+	// 	i++;
+	// }
+	// cout << endl;
+
+	// int i = 1;
+
+	// while (i < 5) {
+	// 	++i;
+	// 	cout << i << " ";
+	// }
+
+	// cout << i << endl;
+
+	return 0;
+}
---
+++ Lecture 3/pattern1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 5;
+
+	for (int row = 1; row <= n; row++) {
+
+		// cout << "I'm at " << row << " row" << endl;
+
+		for (int col = 1; col <= row; col++) {
+			cout << "* ";
+		}
+		cout << endl;
+
+	}
+
+	// cout << "* * * * * * * * * * * * * *. * *" << endl;
+
+	// int n = 56;
+
+	// for (int i = 1; i <= 56; i++) {
+	// 	cout << "* ";
+	// }
+
+	return 0;
+}
