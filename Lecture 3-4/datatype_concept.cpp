2d7ff149d039a3b82e6d1a2b9ba7f7caaf98f781
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-16 22:26:28 +0530

    pattern and datatype

---
+++ Lecture 3-4/datatype_concept.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+	// int x = 5;
+
+	// cout << "INT_MAX ->" << INT_MAX << endl;
+	// cout << "INT_MIN ->" << INT_MIN << endl;
+
+	// // cout << INT_MAX << endl;
+	// // cout << "After updating " << (INT_MAX + 1) << endl;
+
+	// cout << "Before updating " << INT_MIN << endl;
+	// cout << "After updating " << (INT_MIN - 1) << endl;
+
+	// cout << x << endl;
+
+	// cout << sizeof(x) << endl;
+	// cout << sizeof(int) << endl;
+
+	// float
+
+	// bool
+
+	// bool val = true;
+	// bool temp = false;
+
+	// cout << "val " << val << endl;
+	// cout << "temp " << temp << endl;
+
+	//char
+
+	// char ch = 'f';
+
+	// cout << ch << endl;
+
+	// char ch = k wrong
+
+	// char ch = "k"; wrong
+
+	// Conversions
+
+	// int a = 99;
+	// char d = 66;
+	// bool b = 0;
+
+	// cout << "Character " << d << endl; // c
+
+	// cout << "Integer " << a << endl; // 99
+
+	// cout << "Boolean " << b << endl; // 1
+
+	// bool val = -4;
+
+	// cout << val << endl;
+
+	// char pranav = 'k';
+
+	// cout << pranav << endl;
+
+	// int a = pranav;
+
+	// cout << a << endl;
+
+	// bool khandelwal = pranav;
+
+	// cout << khandelwal << endl;
+
+	// char ch = '0';
+
+	// bool val = ch;
+	// cout << val << endl; // 1
+
+	// char c = '9';
+	// int a = c;
+
+	// cout << a << endl;
+
+	// float value = 45.78;
+
+	// int x = value;
+
+	// cout << x << endl;
+
+	return 0;
+}
