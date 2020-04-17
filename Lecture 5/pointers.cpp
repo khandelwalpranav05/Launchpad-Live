cb787975e9a07cf4188383dd71c7326a8bd43a40
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-04-17 22:14:46 +0530

    function and pointers

---
+++ Lecture 5/pointers.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	// int x = 10;
+	// int* xptr = &x;
+
+	// // cout << (&x) << endl;
+	// // cout << xptr << endl;
+
+	// cout << "Values " << endl;
+	// cout << x << endl;
+	// cout << (*xptr) << endl;
+
+	// cout << "Increment 1 to x" << endl;
+
+	// x++;
+
+	// cout << (*xptr) << endl;
+
+	// cout << "Increment 1 to *xptr" << endl;
+
+	// *xptr = *xptr + 1;
+
+	// cout << x << endl;
+
+	cout << "Size of data types" << endl;
+
+	cout << sizeof(int) << endl;
+	cout << sizeof(char) << endl;
+	cout << sizeof(bool) << endl;
+	cout << sizeof(float) << endl;
+
+	cout << "Size of pointers" << endl;
+
+	cout << sizeof(int*) << endl;
+	cout << sizeof(char*) << endl;
+	cout << sizeof(bool*) << endl;
+	cout << sizeof(float*) << endl;
+
+	// float y = 10.56;
+	// float* yptr = &y;
+
+	// cout << y << endl;
+	// cout << (*yptr) << endl;
+
+	// int x =
+	// bool n =
+	// char p =
+	// float f =
+
+	// int*
+	// float*
+	// char*
+	// bool*
+
+	// bool val = true;
+
+	// cout << (&val) << endl;
+
+	return 0;
+}
