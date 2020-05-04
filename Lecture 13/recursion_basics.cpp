3c08581655610e8dc17eace4f5d3376df9721aa8
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-04 20:46:51 +0530

    recursion

---
+++ Lecture 13/recursion_basics.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int n) {
+	// BASE CASE
+	if (n == 0) {
+		return 1;
+	}
+
+	// RECURSIVE
+	int subProblem = factorial(n - 1);
+	int ans = subProblem * n;
+	return ans;
+}
+
+void printIncreasing(int n) {
+	//Base Case
+	if (n == 1) {
+		// cout << 1 << endl;
+		cout << n << endl;
+		return;
+	}
+
+	// Recursive Case
+	printIncreasing(n - 1);
+	cout << n << endl;
+	// return;
+}
+
+void printDecreasing(int n) {
+
+}
+
+int main() {
+
+	// cout << factorial(5) << endl;
+
+	// printIncreasing(5);
+
+	printDecreasing(5);
+
+	return 0;
+}
