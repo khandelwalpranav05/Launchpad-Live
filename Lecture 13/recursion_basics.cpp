7ca79791974c13df52ee48c215a859a4c4a33afe
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-04 22:13:08 +0530

    recursion

--- Lecture 13/recursion_basics.cpp
+++ Lecture 13/recursion_basics.cpp
@@ -29,14 +29,51 @@ void printIncreasing(int n) {
 }
 
 void printDecreasing(int n) {
+	// BASE CASE
+	if (n == 0) {
+		return;
+	}
+
 	// RECURSIVE CASE
 	cout << n << endl;
 	printDecreasing(n - 1);
 	// return;
+}
 
+int power(int n, int p) {
+	//Base
+	if (p == 0) {
+		return 1;
+	}
+
+	//Recursive Case
+	int subProblem = power(n, p - 1);
+	int ans = n * subProblem;
+	return ans;
+}
+
+void function(int parameter) {
+	// BASE CASE (smallest subproblem situation)
+	// will always be the first check priority
+
+	// WORK DONE HERE (BEFORE THE RECURSION CALL) while we are moving
+	// up in the call stack
+
+	function(subproblem); // Recursion CALL
+
+	// WORK DONE HERE (AFTER THE RECURSION CALL) while we are moving
+	// down in the call stack
+}
+
+int linearSearch(int si, int arr[], int n, int key) {
 	// BASE CASE
-	if (n == 0) {
-		return;
+
+	// RECURSIVE CASE
+	if (arr[si] == key) {
+		return si;
+	} else {
+		int index = linearSearch(si + 1, arr, n, key);
+		return index;
 	}
 }
 
@@ -46,7 +83,16 @@ int main() {
 
 	// printIncreasing(5);
 
-	printDecreasing(5);
+	// printDecreasing(5);
+
+	// cout << power(2, 6) << endl;
+
+	int arr[] = {3, 2, 4, 1, 5, 61, 7};
+	int n = 7;
+	int key = 1;
+	int si = 0;
+
+	cout << linearSearch(si, arr, n, key) << endl;
 
 	return 0;
 }
