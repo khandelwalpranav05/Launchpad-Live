0e74474a1dedc8dc2ebb579efbd331a401ad6d27
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-05 20:19:15 +0530

    recursion

--- Lecture 13/recursion_basics.cpp
+++ Lecture 13/recursion_basics.cpp
@@ -51,30 +51,81 @@ int power(int n, int p) {
 	int ans = n * subProblem;
 	return ans;
 }
+/*
+	void function(int parameter) {
+		// BASE CASE (smallest subproblem situation)
+		// will always be the first check priority
 
-void function(int parameter) {
-	// BASE CASE (smallest subproblem situation)
-	// will always be the first check priority
+		// WORK DONE HERE (BEFORE THE RECURSION CALL) while we are moving
+		// up in the call stack
 
-	// WORK DONE HERE (BEFORE THE RECURSION CALL) while we are moving
-	// up in the call stack
+		function(subproblem); // Recursion CALL
 
-	function(subproblem); // Recursion CALL
+		// WORK DONE HERE (AFTER THE RECURSION CALL) while we are moving
+		// down in the call stack
+	}
+*/
 
-	// WORK DONE HERE (AFTER THE RECURSION CALL) while we are moving
-	// down in the call stack
+int sumTillN(int n) {
+	//BASE CASE
+	if (n == 0) {
+		return 0;
+	}
+
+	//RECURSIVE CASE
+	int subproblem = sumTillN(n - 1);
+	int ans = n + subproblem;
+	return ans;
+}
+
+void printDecreasingIncreasing(int n) {
+	if (n == 0) {
+		return;
+	}
+	// if (n == 1) {
+	// 	cout << 1 << endl;
+	// 	cout << 1 << endl;
+	// 	return;
+	// }
+
+	cout << n << endl;
+	printDecreasingIncreasing(n - 1);
+	cout << n << endl;
+}
+
+void printDecreasingODDIncreasingEVEN(int n) {
+	if (n == 0) {
+		return;
+	}
+
+	if (n & 1) { //ODD
+		cout << n << endl; // printing all the decreasing values
+	}
+
+	printDecreasingODDIncreasingEVEN(n - 1);
+
+	if ((n & 1) == 0) { //EVEN
+		cout << n << endl; // printing all the increasing values
+	}
 }
 
 int linearSearch(int si, int arr[], int n, int key) {
 	// BASE CASE
+	if (si == n) {
+		return -1;
+	}
 
 	// RECURSIVE CASE
 	if (arr[si] == key) {
 		return si;
 	} else {
-		int index = linearSearch(si + 1, arr, n, key);
+		int index = linearSearch(si + 1, arr, n, key); // LS on rest of the array
 		return index;
 	}
+}
+
+bool isSorted(int si, int arr[], int n) {
+
 }
 
 int main() {
@@ -86,13 +137,17 @@ int main() {
 	// printDecreasing(5);
 
 	// cout << power(2, 6) << endl;
+	// cout << power(2, 0) << endl;
 
-	int arr[] = {3, 2, 4, 1, 5, 61, 7};
-	int n = 7;
-	int key = 1;
-	int si = 0;
+	// printDecreasingIncreasing(5);
+	// printDecreasingODDIncreasingEVEN(5);
 
-	cout << linearSearch(si, arr, n, key) << endl;
+	// int arr[] = {3, 2, 4, 1, 5, 61, 7};
+	// int n = 7;
+	// int key = 61;
+	// int si = 0;
+
+	// cout << linearSearch(si, arr, n, key) << endl;
 
 	return 0;
 }
