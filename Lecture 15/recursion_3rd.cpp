3600e2f90c3912c35a89187ef24bca817e382754
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-10 21:32:52 +0530

    recursion

--- Lecture 15/recursion_3rd.cpp
+++ Lecture 15/recursion_3rd.cpp
@@ -26,13 +26,42 @@ void printPermutations(string str, string ans) {
 string code[] = {" ", ".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
 void printKeypad(string str, string ans) {
+	if (str.length() == 0) {
+		cout << ans << endl;
+		return;
+	}
+
+	char ch = str[0];
+	string ros = str.substr(1);
+
+	// ch -> 4
+	int idx = ch - '0';
+	string key = code[idx]; // "ghi"
+
+	for (int i = 0; i < key.length(); i++) {
+		printKeypad(ros, ans + key[i]);
+	}
+
+	// recursion("23","g")
+	// recursion("23","h")
+	// recursion("23","i")
+}
+
+void printBoardPath(int start, int end, string path) {
 
 }
 
 int main() {
 
 	// printPermutations("abc", "");
-	printKeypad("23", "");
+	// printKeypad("423", "");
+
+	// 423
+	// 4 -> "qwerty" (6)
+	// 2 -> "abcd" (4)
+	// 3 -> "efg" (3)
+
+	// total = 6*4*3
 
 	return 0;
 }
