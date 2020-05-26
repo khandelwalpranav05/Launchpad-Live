699c579535f232dbc03f79233af73b815b4b2fe2
khandelwalpranav05 <khandelwalpranav1996@gmail.com>
2020-05-26 20:26:00 +0530

    stack

--- Stack/questions.cpp
+++ Stack/questions.cpp
@@ -4,12 +4,49 @@
 using namespace std;
 
 bool isBalanced(string str) {
+	// intuition -> to find the most recent opening bracket in order to
+	// match it with the current closing bracket (what DS we can use which
+	// that helps us to find the most recent value/bracket)
+
+	stack<char> s;
+
+	for (int i = 0; i < str.length(); i++) {
+
+		char ch = str[i];
+
+		if (ch == '(') {
+			s.push(ch);
+		} else {
+
+			// if you're here that means you have a closing
+			// bracket
+
+			if (s.empty()) {
+				return false;
+			}
+
+			s.pop();
+		}
+	}
+
+	// if (s.empty()) {
+	// 	return true;
+	// }
+
+	// return false;
+	return s.empty();
+}
+
+bool isDuplicate(string str) {
 
 }
 
 int main() {
 
-	cout << isBalanced("((()))") << endl;
+	cout << isBalanced("((()))(") << endl; // false
+	cout << isBalanced("(())()") << endl; // true
+	cout << isBalanced("((())") << endl; // false
+	cout << isBalanced("()()()") << endl; // true
 
 	return 0;
 }
